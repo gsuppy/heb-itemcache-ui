45f2f770bddf62d707d7cd1b4fa1c435133d557c
chandranshurao14 <chandranshu.rao@gmail.com>
2020-03-03 17:10:29 -0600

    css for search and nav

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -34,45 +34,49 @@ export class Search extends React.Component<{}, SearchState> {
     render() {
         const query = this.state.query;
         return (
-            <div>
-                <div className="row">
-                    <div className="col-md-6">
-                        <InputGroup>
-                            <FormControl
-                            placeholder="Search"
-                            aria-label="Recipient's username"
-                            aria-describedby="basic-addon2"
-                            value={query}
-                            onChange={this.handleChange}
-                            onKeyPress={(event: { key: string; }) => {
-                                if (event.key === "Enter") {
-                                    if(query != '') this.handleSubmit();
-                                }
-                              }}
-                            />
-                            <InputGroup.Append>
-                            <Button variant="outline-secondary" onClick={this.handleSubmit}>Go</Button>
-                            </InputGroup.Append>
-                        </InputGroup>
-                    </div>
-                    <div className="col-md-6">
-                        <Button variant="outline-secondary">
-                        <i className="fa fa-car"></i>
-                        Shop Curbside
-                        </Button>
-                        <Button variant="outline-secondary">
-                        <i className="fa fa-home"></i>
-                        Shop Delivery
-                        </Button>
-                        <Button bsPrefix="btn heb-button cart">
-                        <i className="fa fa-shopping-cart"></i>
-                        $0.00
-                        </Button>
+            <div className="spacer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-7">
+                            <InputGroup>
+                                <FormControl
+                                placeholder="Search"
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                                value={query}
+                                onChange={this.handleChange}
+                                onKeyPress={(event: { key: string; }) => {
+                                    if (event.key === "Enter") {
+                                        if(query != '') this.handleSubmit();
+                                    }
+                                }}
+                                />
+                                <InputGroup.Append>
+                                <Button variant="outline-secondary" onClick={this.handleSubmit}>Go</Button>
+                                </InputGroup.Append>
+                            </InputGroup>
+                        </div>
+                        <div className="col-md-5">
+                            <Button className="mr-btn" variant="outline-secondary">
+                            <i className="fa fa-car mr"></i>
+                            Shop Curbside
+                            </Button>
+                            <Button className="mr-btn" variant="outline-secondary">
+                            <i className="fa fa-home mr"></i>
+                            Shop Delivery
+                            </Button>
+                            <button type="button" className="btn btn-danger cart">
+                                <i className="fa fa-shopping-cart mr"></i>
+                                $0.00
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                    <Cache cached={this.state.queries}></Cache>
-                    <Queries></Queries>
+                <div className="container-fluid">
+                    <div className="spacer row">
+                        <Cache cached={this.state.queries}></Cache>
+                        <Queries></Queries>
+                    </div>
                 </div>
             </div>
         );
